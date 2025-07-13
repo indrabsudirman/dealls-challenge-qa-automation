56e2af8b603ef66e384309ddefd7a65479a04499
indrabsudirman <indrabsudirman@gmail.com>
2025-07-13 08:54:42 +0700

    remove unused parameter

--- tests/specs/home.spec.ts
+++ tests/specs/home.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test'
 import { HomePage } from '../pages/HomePage'
 
 test.describe('Dealls Mentoring Page', { tag: ['@mentoring', '@smoke', '@regression', '@critical'] }, () => {
-  test('should open Dealls mentoring page and check title', async ({ page, baseURL }) => {
+  test('should open Dealls mentoring page and check title', async ({ page }) => {
     const homePage = new HomePage(page)
 
     await homePage.goto('/mentoring')
